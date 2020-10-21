dbe238485419dca08d034aee15edd7efce7b949b
HKnock <lolu4ka@gmail.com>
2020-10-21 16:05:40 +0300

    Лабораторная работа 8

---
+++ LR08_Orlov_Ivanova/LR08_Orlov_Ivanova/LR08_Orlov_Ivanova.cpp
@@ -0,0 +1,25 @@
+﻿/*Орлов Д.А. и Иванова Ю.А. 
+гр. 591*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double s, t;
+	double x, y, z;
+	cout << "Введите x: ";
+	cin >> x;
+	cout << "Введите y: ";
+	cin >> y;
+	cout << "Введите z: ";
+	cin >> z;
+	cout << "Рассчёт s:" << endl;
+	s = abs(pow(x, x / y) - pow(x / y, 1 / 5));
+	cout << s << endl;
+	cout << "Расчёт t:" << endl;
+	t = (x - y) * (sin(x + y) - tan(y - x) * z) / (1 + pow(x - y, 2));
+	cout << t;
+}
